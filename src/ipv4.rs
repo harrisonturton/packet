9cb3dc3e780d769ee1a63290e43049fc41c8cc46
Harrison Turton <harryt@canva.com>
2023-10-10 10:38:16 +1100

    Finish IPv4 parsing implementation

--- src/ipv4.rs
+++ src/ipv4.rs
@@ -1,18 +1,30 @@
+//! IPv4 packet parsing.
+//!
+//! ## Standards conformance
+//!
+//! This implementation follows the [RFC
+//! 791](https://datatracker.ietf.org/doc/html/rfc791) format as refined by [RFC
+//! 2474](https://datatracker.ietf.org/doc/html/rfc2474) and [RFC
+//! 3168](https://datatracker.ietf.org/doc/html/rfc3168).
+//!
+//! Specifically, it re-interpret the original type-of-service field as the
+//! modern DSCP and ECN replacements.
+//!
+//! 1. [Internet protocol (RFC 791)](https://datatracker.ietf.org/doc/html/rfc791)
+//! 2. [Definition of the Differentiated Services Field (DS Field) in the IPv4 and IPv6 Headers (RFC 2474)](https://datatracker.ietf.org/doc/html/rfc2474)
+//! 3. [The Addition of Explicit Network Congestion Notification (ECN) to IP (RFC 3168)](https://datatracker.ietf.org/doc/html/rfc3168)
 use crate::{bitset, offset_read, Error, Result};
+use std::{mem::size_of, net::Ipv4Addr};
 
-/// An IP packet as defined by RFC 791 and refined by RFC 2474 and RFC 3168. The
-/// payload is in network byte order.
+/// An IPv4 packet.
 ///
-/// This struct wraps a byte array directly. Nothing is parsed until the field
-/// accessor methods (e.g. [`dest`]) are called. Some header values are passed
-/// as copies when they're small, but the payload is always referred to by
-/// reference.
+/// This struct wraps a byte slice directly. Nothing is parsed until the field
+/// accessor methods are called, like [`Packet::dest`]). Some header values are
+/// passed as copies when they're small, but the payload is always referred to
+/// by reference.
 ///
-/// # Relevant RFCs
-/// 1. [Internet protocol (RFC 791)](https://datatracker.ietf.org/doc/html/rfc791)
-/// 2. [Definition of the Differentiated Services Field (DS Field) in the IPv4 and IPv6 Headers (RFC 2474)](https://datatracker.ietf.org/doc/html/rfc2474)
-/// 3. [(RFC 3168)]()
-#[derive(Debug, PartialEq, Eq, Clone)]
+/// See the module documentation for more information.
+#[derive(Debug, PartialEq, Eq, Clone, Hash)]
 pub struct Packet<'a> {
     bytes: &'a [u8],
 }
@@ -31,10 +43,11 @@ impl<'a> Packet<'a> {
     #[inline]
     #[must_use]
     pub fn new(bytes: &'a [u8]) -> Result<Packet> {
-        if bytes.len() < MIN_HEADER_LEN {
-            return Err(Error::InvalidArgument("packet too small"));
+        if bytes.len() >= MIN_HEADER_LEN as usize {
+            Ok(Packet { bytes })
+        } else {
+            Err(Error::CannotParse("packet too small"))
         }
-        Ok(Packet { bytes })
     }
 
     /// Extract the version.
@@ -44,11 +57,15 @@ impl<'a> Packet<'a> {
         unsafe { offset_read::<u8>(self.bytes, 0) >> 4 }
     }
 
-    /// Extract the header length.
+    /// Length of the header in bytes. This is different from the raw field
+    /// contained in the IP packet, which reports the length in increments of
+    /// [`u32`].
     #[inline]
     #[must_use]
     pub fn header_len(&self) -> u8 {
-        unsafe { offset_read::<u8>(self.bytes, 0) & 0xF }
+        // u32 has 4 bytes which is below u8::MAX, so it won't be truncated
+        #[allow(clippy::cast_possible_truncation)]
+        unsafe { (offset_read::<u8>(self.bytes, 0) & 0xF) * size_of::<u32>() as u8 }
     }
 
     /// Extract the differentiate service code point (DSCP).
@@ -61,13 +78,6 @@ impl<'a> Packet<'a> {
     /// Extract the explicit congestion notification field (ECN).
     #[inline]
     #[must_use]
-    pub fn tos<T>(&self) -> Ecn {
-        Ecn::new(unsafe { offset_read::<u8>(self.bytes, 1) & 0b11 })
-    }
-
-    /// Extract the explicit congestion notification field (ECN).
-    #[inline]
-    #[must_use]
     pub fn ecn(&self) -> Ecn {
         Ecn::new(unsafe { offset_read::<u8>(self.bytes, 1) & 0b11 })
     }
@@ -75,29 +85,29 @@ impl<'a> Packet<'a> {
     /// Extract the total length.
     #[inline]
     #[must_use]
-    pub fn total_length(&self) -> u16 {
+    pub fn len(&self) -> u16 {
         u16::from_be_bytes(unsafe { offset_read(self.bytes, 2) })
     }
 
-    /// Extract the identification.
+    /// Extract the identification bits.
     #[inline]
     #[must_use]
-    pub fn ident(&self) -> u16 {
+    pub fn id(&self) -> u16 {
         u16::from_be_bytes(unsafe { offset_read(self.bytes, 4) })
     }
 
     /// Extract the flags.
     #[inline]
     #[must_use]
-    pub fn flags(&self) -> u8 {
-        unsafe { offset_read::<u8>(self.bytes, 6) >> 5 }
+    pub fn flags(&self) -> Flags {
+        Flags::new(unsafe { offset_read::<u8>(self.bytes, 6) >> 5 })
     }
 
     /// Extract the fragment offset.
     #[inline]
     #[must_use]
     pub fn fragment_offset(&self) -> u16 {
-        unsafe { u16::from_be(offset_read::<u16>(self.bytes, 6) & !0x7) }
+        unsafe { u16::from_be(offset_read::<u16>(self.bytes, 6)) & !0xE000 }
     }
 
     /// Extract the time-to-live (TTL).
@@ -117,28 +127,56 @@ impl<'a> Packet<'a> {
     /// Extract the header checksum.
     #[inline]
     #[must_use]
-    pub fn header_checksum(&self) -> [u8; 2] {
+    pub fn checksum(&self) -> [u8; 2] {
         unsafe { offset_read(self.bytes, 10) }
     }
 
     /// Extract the source address.
     #[inline]
     #[must_use]
-    pub fn source(&self) -> [u8; 4] {
-        unsafe { offset_read(self.bytes, 12) }
+    pub fn source(&self) -> Ipv4Addr {
+        Ipv4Addr::from(unsafe { offset_read::<[u8; 4]>(self.bytes, 12) })
     }
 
     /// Extract the destination address.
     #[inline]
     #[must_use]
-    pub fn dest(&self) -> [u8; 4] {
-        unsafe { offset_read(self.bytes, 16) }
+    pub fn dest(&self) -> Ipv4Addr {
+        Ipv4Addr::from(unsafe { offset_read::<[u8; 4]>(self.bytes, 16) })
+    }
+
+    /// Whether the packet has an options field or not
+    #[inline]
+    #[must_use]
+    pub fn has_options(&self) -> bool {
+        self.header_len() - MIN_HEADER_LEN > 0
+    }
+
+    /// Extract the options. You'll have to parse them yourself.
+    #[inline]
+    #[must_use]
+    pub fn options(&self) -> Option<&[u8]> {
+        if !self.has_options() {
+            return None;
+        }
+
+        let start = MIN_HEADER_LEN;
+        let end = start + (self.header_len() - start);
+        Some(&self.bytes[start as usize..end as usize])
+    }
+
+    /// Extract the payload.
+    #[inline]
+    #[must_use]
+    pub fn payload(&self) -> &[u8] {
+        let start = self.header_len() as usize * size_of::<u32>();
+        &self.bytes[start..]
     }
 }
 
 /// Strongly-typed wrapper for "differentiated service code point" (DSCP) field
 /// in the IP packet.
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 pub struct Dscp {
     data: u8,
 }
@@ -162,13 +200,13 @@ impl Dscp {
     #[inline]
     #[must_use]
     pub fn drop_preference(&self) -> u8 {
-        u8::from_be_bytes([(self.data & !0b111001) >> 1])
+        u8::from_be_bytes([(self.data & !0b11_1001) >> 1])
     }
 }
 
 /// Strongly typed wrapper for the explicit congestion notification (ECN) field
 /// in the IP header.
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 pub struct Ecn {
     data: u8,
 }
@@ -196,14 +234,14 @@ impl Ecn {
     }
 }
 
-// Strongly typed wrapped for the "flags" field on the IP packet.
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-pub struct ControlFlags {
+/// Strongly typed wrapper for the "flags" field on the IP packet.
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+pub struct Flags {
     data: u8,
 }
 
-impl ControlFlags {
-    /// Create a new [`ControlFlags`] instance.
+impl Flags {
+    /// Create a new [`Flags`] instance.
     #[inline]
     #[must_use]
     pub fn new(data: u8) -> Self {
@@ -227,17 +265,161 @@ impl ControlFlags {
     }
 }
 
-// Minimum length of an IP packet header.
-const MIN_HEADER_LEN: usize = 20;
+// Minimum length of an IPv4 packet.
+const MIN_HEADER_LEN: u8 = 20;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::{Dscp, Ecn, Flags, Packet};
+    use crate::ethernet::Frame;
+    use std::{error::Error, net::Ipv4Addr};
+
+    // IPv4 packet wrapped in an Ethernet frame, captured using Wireshark.
+    pub const FRAME_WITH_PACKET: &'static [u8] = include_bytes!("../resources/ethernet-ipv4.bin");
+
+    // IPv4 with junk options created using scapy 
+    pub const PACKET_WITH_OPTS: &'static [u8] = include_bytes!("../resources/ipv4-with-options.bin");
 
     #[test]
-    fn packet_has_expected_version() {
-        let dscp = Dscp::new(0b001010);
-        assert_eq!(dscp.forwarding_class(), 1);
+    fn packet_returns_err_when_byte_slice_too_short() -> Result<(), Box<dyn Error>> {
+        let frame = vec![0, 0, 0, 0];
+        let packet = Packet::new(&frame);
+        assert!(packet.is_err());
+        Ok(())
+    }
+
+    #[test]
+    fn packet_has_expected_version() -> Result<(), Box<dyn Error>> {
+        let frame = Frame::new(FRAME_WITH_PACKET)?;
+        let packet = Packet::new(frame.payload())?;
+        assert_eq!(packet.version(), 4);
+        Ok(())
+    }
+
+    #[test]
+    fn packet_has_expected_header_len() -> Result<(), Box<dyn Error>> {
+        let frame = Frame::new(FRAME_WITH_PACKET)?;
+        let packet = Packet::new(frame.payload())?;
+        assert_eq!(packet.header_len(), 20);
+        Ok(())
+    }
+
+    #[test]
+    fn packet_has_expected_dscp() -> Result<(), Box<dyn Error>> {
+        let frame = Frame::new(FRAME_WITH_PACKET)?;
+        let packet = Packet::new(frame.payload())?;
+        assert_eq!(packet.dscp(), Dscp::new(0));
+        Ok(())
+    }
+
+    #[test]
+    fn packet_has_expected_id() -> Result<(), Box<dyn Error>> {
+        let frame = Frame::new(FRAME_WITH_PACKET)?;
+        let packet = Packet::new(frame.payload())?;
+        assert_eq!(packet.id(), 0);
+        Ok(())
+    }
+
+    #[test]
+    fn packet_has_expected_source() -> Result<(), Box<dyn Error>> {
+        let frame = Frame::new(FRAME_WITH_PACKET)?;
+        let packet = Packet::new(frame.payload())?;
+        assert_eq!(packet.source(), Ipv4Addr::new(10, 0, 53, 7));
+        Ok(())
+    }
+
+    #[test]
+    fn packet_has_expected_dest() -> Result<(), Box<dyn Error>> {
+        let frame = Frame::new(FRAME_WITH_PACKET)?;
+        let packet = Packet::new(frame.payload())?;
+        assert_eq!(packet.dest(), Ipv4Addr::new(104, 17, 239, 159));
+        Ok(())
+    }
+
+    #[test]
+    fn packet_has_expected_len() -> Result<(), Box<dyn Error>> {
+        let frame = Frame::new(FRAME_WITH_PACKET)?;
+        let packet = Packet::new(frame.payload())?;
+        assert_eq!(packet.len(), 72);
+        Ok(())
+    }
+
+    #[test]
+    fn packet_has_expected_flags() -> Result<(), Box<dyn Error>> {
+        let frame = Frame::new(FRAME_WITH_PACKET)?;
+        let packet = Packet::new(frame.payload())?;
+        assert_eq!(packet.flags(), Flags::new(0b010));
+        Ok(())
+    }
+
+    #[test]
+    fn packet_has_expected_fragment_offset() -> Result<(), Box<dyn Error>> {
+        let frame = Frame::new(FRAME_WITH_PACKET)?;
+        let packet = Packet::new(frame.payload())?;
+        assert_eq!(packet.fragment_offset(), 0);
+        Ok(())
+    }
+
+    #[test]
+    fn packet_has_expected_checksum() -> Result<(), Box<dyn Error>> {
+        let frame = Frame::new(FRAME_WITH_PACKET)?;
+        let packet = Packet::new(frame.payload())?;
+        assert_eq!(packet.checksum(), [0xA3, 0xED]);
+        Ok(())
+    }
+
+    #[test]
+    fn packet_has_expected_options_when_no_options() -> Result<(), Box<dyn Error>> {
+        let frame = Frame::new(FRAME_WITH_PACKET)?;
+        let packet = Packet::new(frame.payload())?;
+        assert_eq!(packet.options(), None);
+        Ok(())
+    }
+
+    #[test]
+    fn packet_has_expected_has_options_when_no_options() -> Result<(), Box<dyn Error>> {
+        let frame = Frame::new(FRAME_WITH_PACKET)?;
+        let packet = Packet::new(frame.payload())?;
+        assert_eq!(packet.has_options(), false);
+        Ok(())
+    }
+
+    #[test]
+    fn packet_has_expected_options_when_options_exist() -> Result<(), Box<dyn Error>> {
+        let packet = Packet::new(PACKET_WITH_OPTS)?;
+        assert_eq!(packet.options(), Some([1, 10, 0, 0].as_slice()));
+        Ok(())
+    }
+
+    #[test]
+    fn packet_has_expected_has_options_when_options_exist() -> Result<(), Box<dyn Error>> {
+        let packet = Packet::new(PACKET_WITH_OPTS)?;
+        assert_eq!(packet.has_options(), true);
+        Ok(())
+    }
+
+    #[test]
+    fn packet_has_expected_ttl() -> Result<(), Box<dyn Error>> {
+        let frame = Frame::new(FRAME_WITH_PACKET)?;
+        let packet = Packet::new(frame.payload())?;
+        assert_eq!(packet.ttl(), 64);
+        Ok(())
+    }
+
+    #[test]
+    fn packet_has_expected_protocol() -> Result<(), Box<dyn Error>> {
+        let frame = Frame::new(FRAME_WITH_PACKET)?;
+        let packet = Packet::new(frame.payload())?;
+        assert_eq!(packet.protocol(), 17);
+        Ok(())
+    }
+
+    #[test]
+    fn packet_has_expected_ecn() -> Result<(), Box<dyn Error>> {
+        let frame = Frame::new(FRAME_WITH_PACKET)?;
+        let packet = Packet::new(frame.payload())?;
+        assert_eq!(packet.ecn(), Ecn::new(0));
+        Ok(())
     }
 
     #[test]
@@ -271,14 +453,14 @@ mod tests {
     }
 
     #[test]
-    fn control_flags_has_expected_do_not_fragment() {
-        let flags = ControlFlags::new(0b10);
+    fn flags_has_expected_do_not_fragment() {
+        let flags = Flags::new(0b10);
         assert_eq!(flags.do_not_fragment(), false);
     }
 
     #[test]
-    fn control_flags_has_expected_last_fragment_flag() {
-        let flags = ControlFlags::new(0b00);
+    fn flags_has_expected_last_fragment_flag() {
+        let flags = Flags::new(0b00);
         assert_eq!(flags.last_fragment(), true);
     }
 }
