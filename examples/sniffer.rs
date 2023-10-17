5430a0d0afeb31d4a49a6a9167d53474901d5468
Harrison Turton <harryt@canva.com>
2023-10-17 15:08:04 +1100

    Add conditional compilation for sniffer example to compile on every platform

--- examples/sniffer.rs
+++ examples/sniffer.rs
@@ -1,45 +1,59 @@
-use byteorder::{ByteOrder, NetworkEndian};
-use libc::{recv, socket, AF_PACKET, ETH_P_ALL, SOCK_RAW};
-use packet::enet;
-use std::error::Error;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    unsafe {
-        let sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL as u16) as i32);
-        if sock < 0 {
-            println!("socket() failed with error {sock}");
-            return Ok(());
-        }
+#[cfg(target_os = "linux")]
+fn main() {
+    sniffer::run().unwrap();
+}
 
-        let buffer = &mut [0; 65536];
-        loop {
-            let read = recv(sock, buffer.as_mut_ptr() as _, buffer.len(), 0);
+#[cfg(not(target_os = "linux"))]
+fn main() {
+    println!("This example only works on Linux");
+}
 
-            if read < 0 {
-                println!("recv failed with error {read}");
+#[cfg(target_os = "linux")]
+mod sniffer {
+    use libc::{recv, socket, AF_PACKET, ETH_P_ALL, SOCK_RAW};
+    use byteorder::{ByteOrder, NetworkEndian};
+    use packet::enet;
+    use std::error::Error;
+
+    pub fn run() -> Result<(), Box<dyn Error>> {
+        unsafe {
+            let sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL as u16) as i32);
+            if sock < 0 {
+                println!("socket() failed with error {sock}");
                 return Ok(());
             }
 
-            if read == 0 {
-                println!("recv returned 0");
-                return Ok(());
-            }
+            let buffer = &mut [0; 65536];
+            loop {
+                let read = recv(sock, buffer.as_mut_ptr() as _, buffer.len(), 0);
 
-            let frame = enet::Frame::new(&buffer[..read as usize])?;
-            println!(
-                "bytes={:?} source={:?} dest={:?} type={:?}\n{:x?}\n",
-                frame.payload().len(),
-                frame.source(),
-                frame.dest(),
-                frame.length_type(),
-                frame.payload()
-            );
+                if read < 0 {
+                    println!("recv failed with error {read}");
+                    return Ok(());
+                }
+
+                if read == 0 {
+                    println!("recv returned 0");
+                    return Ok(());
+                }
+
+                let frame = enet::Frame::new(&buffer[..read as usize])?;
+                println!(
+                    "bytes={:?} source={:?} dest={:?} type={:?}\n{:x?}\n",
+                    frame.payload().len(),
+                    frame.source(),
+                    frame.dest(),
+                    frame.length_type(),
+                    frame.payload()
+                );
+            }
         }
     }
-}
 
-fn htons(val: u16) -> u16 {
-    let mut bytes = [0, 0];
-    NetworkEndian::write_u16(&mut bytes, val);
-    u16::from_le_bytes(bytes)
-}
+    fn htons(val: u16) -> u16 {
+        let mut bytes = [0, 0];
+        NetworkEndian::write_u16(&mut bytes, val);
+        u16::from_le_bytes(bytes)
+    }
+} 
