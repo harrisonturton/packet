94346e67d67dfcca1e0449a31cdedaef0b6f1cbc
Harrison Turton <harryt@canva.com>
2023-10-17 15:40:45 +1100

    Make parser example produce nicer output

--- examples/parser.rs
+++ examples/parser.rs
@@ -12,24 +12,24 @@ struct Args {
 pub fn main() -> Result<(), Box<dyn Error>> {
     let Args { path } = argh::from_env::<Args>();
 
-    println!("Reading {path}");
     let bytes = std::fs::read(path)?;
-
-    println!("ETHERNET FRAME");
     let frame = enet::Frame::new(&bytes)?;
 
-    println!(
-        "dest: {:x?} src: {:x?} type: {:?} data: {:x?}",
-        frame.dest(),
-        frame.source(),
-        frame.length_type(),
-        frame.payload()
-    );
+    println!("[enet]");
+    println!("length/type = {:?}", frame.length_type());
+    println!("source = {:?}", frame.source());
+    println!("dest = {:?}", frame.dest());
+    println!("payload = {:?}", frame.payload().len());
+    println!("");
 
-    println!("IPv4 PACKET");
     let payload = frame.payload();
     let packet = ipv4::Packet::new(payload)?;
-    println!("{:?} {:?}", packet.source(), packet.dest());
+
+    println!("[ipv4]");
+    println!("protocol = {:?}", packet.protocol());
+    println!("source = {:?}", packet.source());
+    println!("dest = {:?}", packet.dest());
+    println!("payload = {:?}", packet.payload().len());
 
     Ok(())
 }
